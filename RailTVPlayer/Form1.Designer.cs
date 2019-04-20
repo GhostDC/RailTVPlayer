4157de72696301bd81b7a9c531a8abb26f4b14f8
GhostDC <q1007199608@hotmail.com>
2019-04-20 10:21:41 +0800

    增加串口启动方法

--- RailTVPlayer/Form1.Designer.cs
+++ RailTVPlayer/Form1.Designer.cs
@@ -28,13 +28,40 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axShockwaveFlash1 = new AxShockwaveFlashObjects.AxShockwaveFlash();
+            ((System.ComponentModel.ISupportInitialize)(this.axShockwaveFlash1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axShockwaveFlash1
+            // 
+            this.axShockwaveFlash1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.axShockwaveFlash1.Enabled = true;
+            this.axShockwaveFlash1.Location = new System.Drawing.Point(0, 0);
+            this.axShockwaveFlash1.Margin = new System.Windows.Forms.Padding(0);
+            this.axShockwaveFlash1.Name = "axShockwaveFlash1";
+            this.axShockwaveFlash1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axShockwaveFlash1.OcxState")));
+            this.axShockwaveFlash1.Size = new System.Drawing.Size(800, 450);
+            this.axShockwaveFlash1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.axShockwaveFlash1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.axShockwaveFlash1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private AxShockwaveFlashObjects.AxShockwaveFlash axShockwaveFlash1;
     }
 }
 
--- RailTVPlayer/Form1.cs
+++ RailTVPlayer/Form1.cs
@@ -5,16 +5,40 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
+using System.IO;
+using System.IO.Ports;
 
 namespace RailTVPlayer
 {
     public partial class Form1 : Form
     {
+        SerialPort comm = new SerialPort();
+
         public Form1()
         {
             InitializeComponent();
+        }
+        
+        //窗口加载
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            OpenPort();
+        }
+
+        //开启端口
+        void OpenPort()
+        {
+            comm.PortName = "COM3";//定义串口号
+            comm.BaudRate = 9600;//波特率
+            comm.DataBits = 8;//数据位
+            comm.ReadBufferSize = 4096;
+            comm.StopBits = StopBits.One;
+            comm.Parity = Parity.None;
+            comm.Open();
         }
     }
 }
